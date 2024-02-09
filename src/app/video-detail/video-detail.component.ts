cf0dd99d940eef09c6c3d3c452a350d88aaf09a7
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-02-09 19:28:17 +0100

    video detail

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { PopupService } from '../services/popup.service';
+import { environment } from '../../environments/environment.development';
+
 
 @Component({
   selector: 'app-video-detail',
@@ -7,8 +10,51 @@ import { Component, Input } from '@angular/core';
   templateUrl: './video-detail.component.html',
   styleUrl: './video-detail.component.scss'
 })
-export class VideoDetailComponent {
+export class VideoDetailComponent implements OnInit, OnDestroy{
 
-  @Input() videoData:any  
+
+  constructor(public ps:PopupService, private elementRef: ElementRef){}
+  
+  thumb:string = "";
+  video:string = "";
+  resolution: boolean = false;
+
+
+
+
+  ngOnInit(): void {
+      this.thumb = environment.baseUrl + this.ps.videoDetail?.thumbnail;
+      this.video = environment.baseUrl + this.ps.videoDetail?.video_file;
+  }
+
+  ngOnDestroy(): void {
+      this.ps.videoDetail = undefined;
+  }
+
+  changeResolution(path:string , event:MouseEvent){
+    //Video Path zusammenfügen mit Parameter
+
+    event.stopPropagation();
+    this.resolution = false;
+
+  }
+
+
+
+  handleContainerClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.querySelector('.popup').contains(event.target)) {
+        this.closePopup();
+    }
+}
+
+handlePopupClick(event: MouseEvent) {
+    event.stopPropagation();
+}
+
+closePopup() {
+  this.ps.bg = false;
+  this.ps.videoDetail = undefined;
+}
+  
 
 }
